b448d3f89d3b423e7ba59a66c047ddfc4476f7d2
shahraki <shahraki@amnafzar.ir>
2022-05-08 16:49:43 +0430

    Title: display the ship ------------------------------------------------------ Description: display the ship ------------------------------------------------------ Type: feat ------------------------------------------------------ Scope: develop ------------------------------------------------------ Jira: NONE

--- ship_war.py
+++ ship_war.py
@@ -1,18 +1,21 @@
 import sys
 import pygame
 from Settings import Settings
+from ship import Ship
 
 def rungame():
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    ship = Ship(screen)
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
         pygame.display.flip()
 
 rungame()
