fc441a8bb5b9e6ffe0ca7e03687b1de32b563cd3
shahraki <shahraki@amnafzar.ir>
2022-05-08 16:36:50 +0430

    Title: first class ------------------------------------------------------ Description: first class names setting ------------------------------------------------------ Type: feat ------------------------------------------------------ Scope: develop ------------------------------------------------------ Jira: NONE

--- ship_war.py
+++ ship_war.py
@@ -1,15 +1,18 @@
 import sys
 import pygame
+from Settings import Settings
 
 def rungame():
     pygame.init()
-    screen = pygame.display.set_mode((500,500))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        screen.fill(ai_settings.bg_color)
         pygame.display.flip()
 
 rungame()
